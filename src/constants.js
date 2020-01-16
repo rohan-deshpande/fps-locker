c8d5b431c2835caf3ec333f450ab199c2b7fe50f
manthrax <manthrax@gmail.com>
2020-01-15 16:07:07 -0800

    Simplify the API and add a setter for changing fps.

--- src/constants.js
+++
@@ -1,4 +0,0 @@
-export const DEFAULT_FPS = 60;
-export const DEFAULT_MAX_UPDATES = 10;
-export const DEFAULT_ON_UPDATE = () =>
-  console && console.warn("You have not provided an onUpdate method");
--- src/index.js
+++ src/index.js
@@ -1,42 +1,34 @@
-import {
-  DEFAULT_FPS,
-  DEFAULT_MAX_UPDATES,
-  DEFAULT_ON_UPDATE,
-} from './constants';
 
 export default class FpsLocker {
+  set fps( fps ){
+  	this._fps = fps;
+  	this.timeSlice = 1000 / fps
+  }
+  reset(){
+  	this.cumulativeTime = this.lastTime = 0
+  }
   constructor(
-    onUpdate = DEFAULT_ON_UPDATE,
-    fps = DEFAULT_FPS,
-    maxUpdates = DEFAULT_MAX_UPDATES
-  ) {
-    this.cumulativeTime = 0;
-    this.timeSlice = 1000 / fps;
+    onUpdate = () => console && console.warn("You have not provided an onUpdate method"),
+    fps = 60,
+    maxUpdates = 10
+  ){
+  	this.fps = fps;
     this.maxUpdates = maxUpdates;
     this.onUpdate = onUpdate;
+  	this.reset()
   }
-
   update(now = performance.now()) {
     let numUpdates = 1;
-
     if (!this.lastTime) {
-      this.lastTime = now;
+      	this.lastTime = now;
     } else {
       const elapsed = now - this.lastTime;
-
       this.cumulativeTime += elapsed;
       numUpdates = (this.cumulativeTime / this.timeSlice) | 0;
       this.cumulativeTime -= numUpdates * this.timeSlice;
-
-      if (numUpdates > this.maxUpdates) {
-        numUpdates = this.maxUpdates;
-      }
-
+      if (numUpdates > this.maxUpdates) numUpdates = this.maxUpdates;
       this.lastTime = now;
     }
-
-    while (numUpdates-- > 0) {
-      this.onUpdate();
-    }
+    while (numUpdates-- > 0) this.onUpdate();
   }
 }
